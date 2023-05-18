0f699fcfc639b902b5db4df8896e882ca2d5af40
vudinhan2525 <vudinhan000@gmail.com>
2023-05-18 07:43:14 +0700

    test

---
+++ Test-pop-up-when-click/main.js
@@ -0,0 +1,9 @@
+/**
+ * Sử dụng các biến đã cho sẵn dưới đây
+ */
+
+var productsListElement = document.querySelector('.products-list ');
+var firstProductElement = document.querySelector('.products-list .product');
+var buttonElements = document.getElementsByClassName('button');
+console.log(productsListElement);
+
